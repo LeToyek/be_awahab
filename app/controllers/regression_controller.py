1f84c34f26f696c194e5450008f59ca16d23a04a
LeToyek <maulanatoyek@gmail.com>
2024-08-15 14:49:53 +0700

    fix(input): add separator param

---
+++ app/controllers/regression_controller.py
@@ -0,0 +1,29 @@
+from flask import Blueprint, request, jsonify
+from app.services.regression_service import RegressionService
+
+# Create a Blueprint for the regression routes
+regression_bp = Blueprint('regression', __name__)
+
+# Create an instance of RegressionService
+regression_service = RegressionService()
+
+@regression_bp.route('/predict', methods=['POST'])
+def predict():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file provided'}), 400
+    
+    file = request.files['file']
+    
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+    
+    try:
+        # Perform prediction from the file
+        res_ti,res_si = regression_service.predict_from_file(file)
+        json = {
+            'TI': res_ti,
+            'SI': res_si
+        }
+        return jsonify(json)
+    except ValueError as e:
+        return jsonify({'error': str(e)}), 400
---
+++ app/services/regression_service.py
@@ -0,0 +1,58 @@
+import numpy as np
+from app.models.regression_model import RegressionModel
+from app.utils.data_utils import read_file, preprocess_data,get_specific_df,determine_target
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+
+class RegressionService:
+    def __init__(self):
+        self.model = RegressionModel()
+        self.scaler = StandardScaler()
+    
+    def predict(self, X, y):
+        X = np.array(X).reshape(-1, 1)
+        y = np.array(y)
+        self.model.fit(X, y)
+        return self.model.predict(X)
+    
+    
+    def predict_from_file(self, file):
+        df = read_file(file)
+        print(f"Preprocessed data: {df}")
+        
+        preprocessed =  preprocess_data(df)
+        print(f"Preprocessed data: {preprocessed}")
+        
+        df_ti = get_specific_df(preprocessed,'TI')
+        df_si = get_specific_df(preprocessed,'SI')
+        
+        res_ti = self.get_predicted_data(df_ti,'Jml_Mhs_TI')
+        res_si = self.get_predicted_data(df_si,'Jml_Mhs_SI')
+        
+        return res_ti,res_si
+    
+    def get_predicted_data(self,df,target):
+        x,y = determine_target(df,target)
+        
+        X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+        X_train_scaled = self.scaler.fit_transform(X_train)
+        X_test_scaled = self.scaler.transform(X_test)
+        
+        self.model.fit(X_train_scaled, y_train)
+        
+        predictions = self.model.predict(X_test_scaled)
+        
+        r2 = r2_score(y_test, predictions)
+        mse = mean_squared_error(y_test, predictions)
+        mae = mean_absolute_error(y_test, predictions)
+        return {
+            'r2': r2,
+            'mse': mse,
+            'mae': mae,
+            'predictions': predictions.tolist()
+        }
+    
+        
+        
