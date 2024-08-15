eab7f718da700baec56a0709ecdc913e09192a1d
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2024-08-15 22:26:39 +0700

    feat(app): add cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
-from .controllers.regression_controller import regression_bp
+from flask_cors import CORS
+from app.controllers.regression_controller import regression_bp
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object('app.config.Config')
+    
     
     # Register Blueprints
     app.register_blueprint(regression_bp, url_prefix='/regression')
